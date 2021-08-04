5e5ce5ca94873ed7f3eb14af6caa4374bea152f0
rr1399 <rahul13rupesh@gmail.com>
2021-08-04 11:33:08 +0530

    first commit

---
+++ src/main/java/com/institute/businessplanner/BusinessplannerApplication.java
@@ -0,0 +1,14 @@
+package com.institute.businessplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessplannerApplication.class, args);
+		System.out.println("Inside business planner");
+	}
+
+}
